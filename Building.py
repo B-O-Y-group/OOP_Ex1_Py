672818ee2fb2e1b0480c92ebed6765e6d5561f28
yosefkahlon <yosefkahlon53@gmail.com>
2021-11-16 18:05:20 +0200

    building

--- Building.py
+++ Building.py
@@ -6,37 +6,38 @@ import json
 
 class Building:
 
-    def __init__(self, file):
+    def __init__(self, min_floor, max_floor):
 
         self.count = 0  # how many elevator
+        self.min_floor = min_floor
+        self.max_floor = max_floor
+        self.elevator = []
+
+    def load(self, file):
         try:
             with open('file', 'r+'):
                 data = json.loads(file)
 
                 self.min_floor = data["_minFloor"]
                 self.max_floor = data["_maxFloor"]
-                self.elevator = []
-
 
                 for i in data['_elevators']:
-                 #  elev = Elevator("_id", "_speed", "_minFloor", "_maxFloor", "_closeTime", "_openTime",
-                  #     "_startTime",
-                           #     "_stopTime")
-                        print(i)
-                     #   self.elevator.append(Elevator(i))
-              #  self.ElevatorList = self.elevator
+                    elev = Elevator("_id", "_speed", "_minFloor", "_maxFloor", "_closeTime", "_openTime", "_startTime",
+                                    "_stopTime")
+
+                self.elevator.append(elev)
+            self.ElevatorList = self.elevator
 
 
         except IOError as exp:
-             print(exp)
-
+            print(exp)
 
     def __str__(self) -> str:
-         return f"{self.elevator}\n cont:{self.count}"
+        return f"{self.elevator}\n cont:{self.count}"
 
-# if __name__ == '__main__':
-#     file = r"Users/97252/Downloads/Ex1_input (1)/Ex1_input/Ex1_Buildings/B1.json"
-#
-#     b1 = Building(file)
-#
-#     print(b1.max_Floor)
+if __name__ == '__main__':
+    file = r"Users/97252/Downloads/Ex1_input (1)/Ex1_input/Ex1_Buildings/B1.json"
+
+    b1 = Building(file)
+
+    print(b1.max_Floor)
--- Elevator.py
+++ Elevator.py
@@ -3,15 +3,15 @@
 class Elevator:
 
 
-    def __init__(self, d) : #id, speed, min_floor, max_floor, close_time, open_time, start_time, stop_time):
-        self.id = d['_id']
-        self.speed = d['_speed']
-        self.min_floor = d['_minFloor']
-        self.max_floor = d['_maxFloor']
-        self.close_time = d['_closeTime']
-        self.open_time = d['_openTime']
-        self.start_time = d['_startTime']
-        self.stop_time = d['_stopTime']
+    def __init__(self, _id, _speed, _minFloor, _maxFloor, _closeTime, _openTime, _startTime, _stopTime):
+        self.id = id
+        self.speed = _speed
+        self.min_floor =_minFloor
+        self.max_floor =_maxFloor
+        self.close_time = _closeTime
+        self.open_time = _openTime
+        self.start_time = _startTime
+        self.stop_time = _stopTime
 
 
 
@@ -35,4 +35,3 @@ class Elevator:
 
     def stop_time(self):
         self.stop_time()
-        
