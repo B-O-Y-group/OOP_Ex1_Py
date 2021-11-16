2df76213351506fefacd89710899c02ea30b6425
orondovev <123dovev@gmail.com>
2021-11-16 17:15:12 +0200

    t_node and e_range in progress

---
+++ E_Range.py
@@ -0,0 +1,14 @@
+# from B_Traffic import *
+# from binarytree import *
+# import binarytree
+import T_Node
+# binarytree.bst()
+
+
+class e_range:
+
+
+    def divide(self):
+        T_Node.set_split()
+
+
