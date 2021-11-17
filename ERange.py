e6342abb08693ebb1596197e49783b35993b3553
orondovev <123dovev@gmail.com>
2021-11-17 22:49:54 +0200

    change TNode

--- ERange.py
+++ ERange.py
@@ -5,13 +5,17 @@ from TNode import *
 from Esort import *
 from BTraffic import *
 
+
 class ERange:
 
-    def __init__(self, b_traffic: BTraffic, e_sort: Esort):
+
+# def __init__(self, b_traffic: BTraffic, e_sort: Esort):
 
 
 
 
 
-
-
+if __name__ == "__main__":
+    i = 1
+    j = 15
+    next_val = 21
--- TNode.py
+++ TNode.py
@@ -1,9 +1,9 @@
-from binarytree import Node
-
+from binarytree import *
+import numpy as np
 from BTraffic import *
 
 
-class T_Node:
+class TNode:
 
     def __init__(self, left, right, value):
         self.range = {"i": left, "j": right}
@@ -11,6 +11,21 @@ class T_Node:
         self.flag: bool = True
 
         self.split = {"x": left, "y": right}
+
+    def set_split(self, i, j, target):
+        self.target = target
+        i = 
+        while
+        # sum_val = 0
+        # count = 1
+        # for a in range(i, j + 1):
+        #     sum_val = sum_val + a
+        #     count += 1
+        #     if a > target:
+        #         sum_val - a,
+        #         break
+        # return sum_val, count-1
+
 
 
     def allocate(self, x, y):
