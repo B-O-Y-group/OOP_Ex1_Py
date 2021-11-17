52a859d241ad8073df2b9ebd21b401ecf649bb48
orondovev <123dovev@gmail.com>
2021-11-17 20:49:26 +0200

    add max_heap

--- ERange.py
+++ ERange.py
@@ -1,11 +1,14 @@
-# from BTraffic import *
-# from binarytree import *
-# from TNode import *
-#
-#
-# class ERange(BTraffic):
-#
-#     def __init__(self):
+import Esort
+from BTraffic import *
+from binarytree import bst
+from TNode import *
+from Esort import *
+from BTraffic import *
+
+class ERange:
+
+    def __init__(self, b_traffic: BTraffic, e_sort: Esort):
+
 
 
 
--- TNode.py
+++ TNode.py
@@ -1,20 +1,20 @@
 from binarytree import Node
 
+from BTraffic import *
+
 
 class T_Node:
 
-    def __init__(self, left, right, traffic):
-        in_range = {"i": left, "j": right}
-
-        self.traffic = traffic
-
-        flag: bool = True
+    def __init__(self, left, right, value):
+        self.range = {"i": left, "j": right}
+        self.value = value
+        self.flag: bool = True
 
         self.split = {"x": left, "y": right}
 
 
-    def set_false(self, x, y):
-        flag = False
+    def allocate(self, x, y):
+        self.flag = False
         self.set_split(x, y)
 
     def set_split(self, x, y):
